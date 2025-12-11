d8be9000241884fa8aa44bbf6daf6ea19a141d42
Robert John Shelford <robert.shelford@googlemail.com>
2025-12-11 12:54:06 +0000

    feat(post_detail): add post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest
 from django.views import generic
 from .models import Post
 
@@ -16,3 +17,26 @@ class PostList(generic.ListView):
         print("DEBUG page_obj: ", page_obj)
 
         return context
+
+
+def post_detail(request: HttpRequest, slug):
+    """Display an individual :model:`blog.Post`
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`
+
+    **Template**
+
+    :template:`blog/post_detail.html`.
+
+    Args:
+        request (HttpRequest): The incoming HttpRequest object
+        slug (str): The slug to use to for the post get_object_or_404 method
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(request, "blog/post_detail.html", {"post": post})
